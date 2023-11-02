570191738a9df455ef62f167a2e37714ecc2f84e
Anchorish <hrishabh@anchorenvironmental.co.za>
2023-11-02 09:14:11 +0200

    testing folium

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,14 @@
+import pandas as pd
+import geopandas
+import matplotlib.pyplot as plt
+from geodatasets import get_path
+import folium
+
 def main():
- print("Hello World")
+    # world = geopandas.read_file(get_path("naturalearth.land"))
+    map = folium.Map(location=[13.406, 80.110], tiles="CartoDB Positron", zoom_start=9)
+    # ax = map.plot()
+    plt.show()
 
 if __name__ == "__main__":
     main()
