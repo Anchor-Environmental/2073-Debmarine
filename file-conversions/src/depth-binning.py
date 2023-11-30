9fc6cea12fc7bb93baaaa00d9fb810d5b85875cd
Anchorish <hrishabh@anchorenvironmental.co.za>
2023-11-30 09:07:54 +0200

    dec-jan conversion

--- file-conversions/src/depth-binning.py
+++ file-conversions/src/depth-binning.py
@@ -8,7 +8,7 @@ import math
 
 
 files_to_read = []
-chunk_length = int(30*4) # size of 
+chunk_length = int(31*4*2) # number of days in month * number of intervals in each day
 
 raw_data_depth_array = [0.4940249, 
                1.541375,
@@ -41,8 +41,8 @@ raw_data_layer_thickness = []
 
 g = 9.81
 
-output_time = np.linspace(0.0, 60*6*119, num=chunk_length)
-
+output_time = np.linspace(0.0, 60*6*247, num=chunk_length)
+print(output_time)
 prevValue=0
 for value in raw_data_depth_array:
   raw_data_layer_thickness.append(value-prevValue)
@@ -68,7 +68,7 @@ def main():
     
   format_file.close()
 
-  for file_name in os.listdir('./file-conversions/data/test_convert/'):
+  for file_name in os.listdir('./file-conversions/data/dec_jan_convert/'):
     if "Boundry" in file_name:
       files_to_read.append(file_name) 
 
@@ -110,7 +110,7 @@ def read_xlsx(file):
   print(f'Current File: {file}')
   print(f"Chunk size: {chunk_length}")
   
-  pd_data = pd.read_excel(f'./file-conversions/data/test_convert/{file}', sheet_name = 0, index_col = 0)
+  pd_data = pd.read_excel(f'./file-conversions/data/dec_jan_convert/{file}', sheet_name = 0, index_col = 0)
 
   for current_chunk in range(int(len(pd_data)/chunk_length)):
     
