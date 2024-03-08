1c061d773e486867dbf7a1f9e4d2c57a7fccd6ce
Hrishabh Rajeev <hrishabh@anchorenvironmental.co.za>
2024-03-08 13:56:14 +0200

    new file directory

--- file-conversions/src/depth-binning.py
+++ file-conversions/src/depth-binning.py
@@ -70,7 +70,7 @@ def main():
     
   format_file.close()
 
-  for file_name in os.listdir('./file-conversions/data/jan2022_june2023_vp'):
+  for file_name in os.listdir('./file-conversions/data/jan2022_june2023_magnitude'):
     if "Boundry" in file_name:
       files_to_read.append(file_name) 
 
@@ -115,7 +115,7 @@ def read_xlsx(file):
   print(f'Current File: {file}')
   print(f"Chunk size: {chunk_length}")
   
-  pd_data = pd.read_excel(f'./file-conversions/data/jan2022_june2023_vp/{file}', sheet_name = 0, index_col = 0)
+  pd_data = pd.read_excel(f'./file-conversions/data/jan2022_june2023_magnitude/{file}', sheet_name = 0, index_col = 0)
 
   for current_chunk in range(int(len(pd_data)/chunk_length)):
     
