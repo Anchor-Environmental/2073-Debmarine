53fcb018205e7a54dc73bc8f02b41cf44e6ee219
Hrishabh Rajeev <hrishabh@anchorenvironmental.co.za>
2024-04-24 12:56:26 +0200

    implementation done

--- file-conversions/src/test.py
+++ file-conversions/src/test.py
@@ -2,11 +2,21 @@ import numpy as np
 
 start_dredge_time = 36000
 dredge_days = 7
-dredge_zone = 1
+dredge_zone = 30
 mins_in_day = 1440
+num_dredge_cells = 30
 end_dredge_time = dredge_days * mins_in_day + start_dredge_time
 loops = end_dredge_time - start_dredge_time
 discharge_interval = 48
+start_flow_rate = 0.073
+stop_flow_rate = 0
+
+start_flow = np.zeros(dredge_days)
+stop_flow = np.zeros(dredge_days)
+start_time = np.zeros(dredge_days)
+stop_time = np.zeros(dredge_days)
+complete_discharge_list = np.zeros(dredge_days*2)
+discharge_flow_rates = np.zeros(dredge_days*2)
 
 def main():
 
@@ -14,18 +24,36 @@ def main():
 
     if i%2 == 0:
 
-      start_time_1 = start_dredge_time + discharge_interval*(dredge_zone-1) + mins_in_day*i
-      stop_time_1 = start_dredge_time + discharge_interval*dredge_zone + mins_in_day*i
+      start_time[i] = start_dredge_time +  discharge_interval*(dredge_zone-1) + (discharge_interval * num_dredge_cells * i)
+      stop_time[i] = start_dredge_time + discharge_interval*(dredge_zone) + (discharge_interval * num_dredge_cells * i)
 
-      print(f'start time : {start_time_1}; stop time : {stop_time_1}')
+      start_flow[i] = start_flow_rate
+      stop_flow[i] = stop_flow_rate
+
+      # print(f'day : {i+1}; start time : {start_time[i]}; stop time : {stop_time[i]}')
 
     else:
       
-      start_time_2 = start_dredge_time - discharge_interval*(dredge_zone) + mins_in_day*(i+1)
-      stop_time_2 = start_dredge_time + mins_in_day*(i+1)
+      start_time[i] = start_dredge_time +  discharge_interval*(num_dredge_cells-dredge_zone) + (discharge_interval * num_dredge_cells * i)
+      stop_time[i] = start_dredge_time + discharge_interval*(num_dredge_cells-(dredge_zone - 1)) + (discharge_interval * num_dredge_cells * i)
 
-      print(f'start time : {start_time_2}; stop time: {stop_time_2}')
+      start_flow[i] = start_flow_rate
+      stop_flow[i] = stop_flow_rate
 
+      # print(f'day : {i+1}; start time : {start_time[i]}; stop time: {stop_time[i]}')
+
+
+  complete_discharge_list = np.stack((start_time, stop_time), axis=1)
+  discharge_flow_rates = np.stack((start_flow, stop_flow), axis=1)
+
+  complete_discharge_list = complete_discharge_list.flatten()
+  # discharge_flow_rates = discharge_flow_rates.flatten()
+
+  # complete_discharge_list = np.stack((complete_discharge_list, discharge_flow_rates), axis=1)
+
+  print(complete_discharge_list)
+
+  np.savetxt(f"./file-conversions/out/discharges/dredgezones/DredgeZone.csv", complete_discharge_list)  
 
 
 if __name__ == "__main__":
